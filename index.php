7ed14d7378863830b641c3ed43754f338bb81493
Lucas Stein <66976832+steinlucas@users.noreply.github.com>
2023-02-08 21:39:13 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <form method="post">
+    <form method="post" action="sucesso.php">
         <a>Nome completo</a>
-        <input type="text" id="nome" name="nome">
+        <input required type="text" id="nome" name="nome">
 
         <br><br>
 
         <a>E-mail</a>
-        <input type="email" id="email" name="email">
+        <input required type="email" id="email" name="email">
 
         <br><br>
 
         <a>Sexo</a>
-        <select name="sexo">
+        <select required name="sexo">
             <option value="masculino">Masculino</option>
             <option value="feminino">Feminino</option>
             <option value="outro">Outro</option>
@@ -25,7 +27,7 @@
         <br><br>
 
         <a>Curso</a>
-        <select name="curso">
+        <select required name="curso">
             <option value="programadorWeb">Programador Web</option>
             <option value="bancoDados">Banco de dados</option>
             <option value="cienciaDados">Ciência de dados</option>
@@ -34,17 +36,17 @@
         <br><br>
 
         <a>Endereço</a>
-        <input type="text" id="endereco" name="endereco">
+        <input required type="text" id="endereco" name="endereco">
 
         <br><br>
         
         <a>CEP</a>
-        <input type="number" id="cep" name="cep">
+        <input required type="number" max="7" minlength="7" id="cep" name="cep">
 
         <br><br>
         
         <a>Município</a>
-        <input type="text" id="municipio" name="municipio">
+        <input required type="text" id="municipio" name="municipio">
 
         <br><br>
 
@@ -52,7 +54,7 @@
         ?>
 
         <a>Estado</a>
-        <select name="estado">
+        <select required name="estado">
         <?php
         foreach ($estados as $k=> $estado) {
             ?> <option value="<?php echo $k; ?>"><?php echo $estado; ?></option> 
